d591a56501aa533b4c96a42d2ed6a700669cd123
yongtkim <kyt3696@gmail.com>
2025-01-17 20:48:21 +0900

    스크립트 compiler 추가

---
+++ js/excuteUtils.js
@@ -0,0 +1,85 @@
+var isDark = false, isHorizontal = false;
+/*
+* Convert Theme
+* dark <-> white
+* */
+var convertTheme = () => {
+  if(isDark){
+    isDark = false;
+    replaceClassName(document, 'bg-dark', 'bg-white');
+    replaceClassName(document, 'text-white', 'text-dark');
+    replaceClassName(document, 'btn-light', 'btn-dark');
+  } else {
+    isDark = true;
+    replaceClassName(document, 'bg-white', 'bg-dark');
+    replaceClassName(document, 'text-dark', 'text-white');
+    replaceClassName(document, 'btn-dark', 'btn-light');
+  }
+}
+/*
+* Convert rotate
+* vertical <-> horizontal
+* */
+var convertRotate = () => {
+  var container = document.querySelector('#bodyContainer');
+  if(isHorizontal){
+    isHorizontal = false;
+    replaceClassName(container, 'col-6', 'col-12');
+  } else {
+    isHorizontal = true;
+    replaceClassName(container, 'col-12', 'col-6');
+  }
+}
+/*
+* ClassName Replace
+* as-is : ClassName(arg1)
+* to-be : ClassName(arg2)
+* */
+var replaceClassName = (target, asIS, toBe) => {
+  var targetArr = target.getElementsByClassName(asIS);
+  if(targetArr != undefined){
+    var length = targetArr.length;
+    for(var i = length-1; i >= 0; i--) {
+      var className = targetArr[i].className;
+      targetArr[i].className = className.replaceAll(asIS, toBe);
+    }
+  }
+}
+/*
+* XMLHttpRequest
+* method : GET, POST
+* */
+var xhrRequest = function (method, uri, data, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        callback(xhr.responseText);
+      }
+    }
+  };
+  xhr.open(method, uri, true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send(data);
+}
+/*
+* Compile
+* lib : https://closure-compiler.appspot.com/compile
+* */
+var optimize = () => {
+  var s = document.querySelector('#textareaContainer textarea').value;
+  var jsonData = {
+    js_code: s,
+    compilation_level: 'WHITESPACE_ONLY',
+    output_format: 'text',
+    output_info: 'compiled_code'
+  }
+  xhrRequest('POST', 'https://closure-compiler.appspot.com/compile', Object.keys(jsonData).map(key => `${key}=${jsonData[key]}`).join('&'), function (e) {
+    document.querySelector('#textContainer').innerText = e;
+  });
+}
+
+var copy = () => {
+  var text = document.querySelector('#textContainer').innerText;
+  navigator.clipboard.writeText(text);
+}
---
+++ js/gridUtils.js
@@ -0,0 +1,39 @@
+var convertType = "";
+var excuteConvert = () => {
+  switch (convertType) {
+    case "url":
+      excuteUrl();
+      break;
+    case "script":
+    case "documnet":
+    default:
+      excuteScript();
+      break;
+  }
+};
+var excuteUrl = () => {
+  var s = document.querySelector('#textareaContainer textarea').value;
+  var f = document.querySelector('#iframeContainer iframe');
+  var ifrm = document.createElement('iframe');
+  ifrm.src = s;
+  ifrm.width = "100%";
+  ifrm.height = "100%";
+  ifrm.style = "position: fixed;top: 0;left: 0;border: none;z-index:99999";
+  f.src = s;
+};
+var excuteScript = () => {
+  var s = document.querySelector('#textareaContainer textarea').value;
+  var f = document.querySelector('#iframeContainer iframe');
+  var innerDocument = '<html><head><script src="https://code.jquery.com/jquery-latest.min.js"><\/script></head><body>';
+  innerDocument += s;
+  innerDocument += '</body>';
+
+  f.contentDocument.write(innerDocument);
+  f.contentDocument.close();
+};
+
+
+var selectTYpe = (text) => {
+  document.querySelector('#selectType').innerText = text;
+  convertType = text;
+};
